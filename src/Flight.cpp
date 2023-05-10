1dc69d619505777b6381f7d4d960d940ea98a493
marius004 <scarlatmariusstefan2018@gmail.com>
2023-05-10 12:36:00 +0300

    fix

--- src/Flight.cpp
+++ src/Flight.cpp
@@ -67,17 +67,10 @@ Flight::Flight(const Flight &flight)
       source(flight.source),
       destination(flight.destination),
       aircraft(flight.aircraft),
-      status(flight.status)
-    {
-
-    this->passengers = {};
-//    for (const auto& passenger : passengers)
-//        this->passengers.push_back(std::shared_ptr<Passenger>(passenger));
-
-    this->crew = {};
-//    for (const auto& member : crew)
-//        this->crew.push_back(std::shared_ptr<AircraftCrewMember>(member));
-}
+      status(flight.status),
+      passengers(flight.passengers),
+      crew(flight.crew)
+    {}
 
 Flight &Flight::operator=(const Flight &other) {
     if (this != &other) {
